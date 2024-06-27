9ddaa2bbc5775d1688102ec348f1c8fdb10fc46a
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-27 16:37:39 +0200

    feat: added som coments

--- main.js
+++ main.js
@@ -9,22 +9,18 @@ createApp({
 
         }
     },
-
     mounted () {
-       
+        // faccio un ciclo che mi generi 10 emementi 
         for ( let i = 0; i < 10; i++){
              // rischiesta email al server 
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail ')
             // associo la mail generata alla mail nel mio data
             .then ((mailUser) => {
-                const result = mailUser.data.response           ;
+                const result = mailUser.data.response;
                 console.log(mailUser);
                 this.email.push(result);
-    
             })
         }
-       
-        
     },
 
     //funzioni methods dell App    
